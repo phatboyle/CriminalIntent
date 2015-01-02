b002f08e776f54e74985cc378fe2275780b40698
Pat Boyle <patboyle@Pats-iMac.local>
2015-01-02 14:10:04 -0800

    initial commit

---
+++ app/src/main/java/com/example/patboyle/criminalintent/CrimeActivity.java
@@ -0,0 +1,20 @@
+package com.example.patboyle.criminalintent;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+
+public class CrimeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+
+
+
+
+}
